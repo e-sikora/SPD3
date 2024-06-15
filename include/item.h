1c423a9dd055b738af86e411c9765f361e723bf5
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2024-06-15 20:03:33 +0200

    Add automatic documentation
    - add comments to header files
    - configure Doxyfile

--- include/item.h
+++ include/item.h
@@ -6,8 +6,7 @@
 #include <numeric>
 
 /**
- * @brief A class representing an item with attributes like ID, occurrence time,
- * work time, and idle time.
+ * @brief A class representing an item with attributes like ID and work time.
  *
  * @tparam T The type of the item (currently unused).
  */
@@ -16,11 +15,20 @@ class Item
 {
 private:
     int id;         ///< The unique identifier of the item.
-    std::vector<int> work_time;
+    std::vector<int> work_time; ///< Vector containing work times.
 
 public:
+    /**
+     * @brief Default constructor initializes the item with ID 0 and empty work time.
+     */
     Item() : id(0), work_time() {}
 
+    /**
+     * @brief Parameterized constructor initializes the item with given ID and work time.
+     *
+     * @param id_s The ID of the item.
+     * @param work_time_s The vector containing work times.
+     */
     Item(int id_s, std::vector<int> work_time_s)
     {
         id = id_s;
@@ -33,17 +41,33 @@ public:
      * @return int The ID of the item.
      */
     int getId() const { return id; }
-    
+
+    /**
+     * @brief Getter method for the work time of the item.
+     *
+     * @return std::vector<int> The work time of the item.
+     */
     std::vector<int> getWorkTime() const { return work_time; }
 
+    /**
+     * @brief Calculates the sum of all work times.
+     *
+     * @return int The sum of all work times.
+     */
     int getSumWorkTime() const { return std::accumulate(work_time.begin(), work_time.end(), 0); }
 
+    /**
+     * @brief Getter method for a specific work time by index.
+     *
+     * @param index The index of the work time to retrieve.
+     * @return int The work time at the specified index.
+     */
     int getOneWorkTime(int index) const { return work_time[index]; }
 
     /**
-     * @brief Getter method for the idle time associated with the item.
+     * @brief Setter method for the work time of the item.
      *
-     * @return int The idle time associated with the item.
+     * @param work_time_s The new work time to set.
      */
     void setWorkTime(std::vector<int> work_time_s) { work_time = work_time_s; }
 
@@ -56,40 +80,39 @@ public:
      */
     bool operator<(const Item &other) const { return id < other.id; }
 
+    /**
+     * @brief Compares the first work time of this item with another item.
+     *
+     * @param other The other item to compare with.
+     * @return true If this item's first work time is less than the other item's first work time.
+     * @return false Otherwise.
+     */
     bool compareByFirstWorkTime(const Item &other) const;
 
+    /**
+     * @brief Compares the second work time of this item with another item.
+     *
+     * @param other The other item to compare with.
+     * @return true If this item's second work time is less than the other item's second work time.
+     * @return false Otherwise.
+     */
     bool compareBySecondWorkTime(const Item &other) const;
 
+    /**
+     * @brief Compares all work times of this item with another item.
+     *
+     * @param other The other item to compare with.
+     * @return true If this item's work times are less than the other item's work times.
+     * @return false Otherwise.
+     */
     bool compareByAllWorkTime(const Item &other) const;
 
-    bool operator==(const Item &other) const; 
+    /**
+     * @brief Overloaded equality operator for comparing items based on their IDs.
+     *
+     * @param other The other item to compare with.
+     * @return true If the items have the same ID.
+     * @return false Otherwise.
+     */
+    bool operator==(const Item &other) const;
 };
-
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-
-#include "doctest/doctest.h"
-
-TEST_CASE("Item class")
-{
-    // // Default constructor
-    // Item<int> defaultItem;
-    // CHECK(defaultItem.getId() == 0);
-    // CHECK(defaultItem.getOccurTime() == 0);
-    // CHECK(defaultItem.getWorkTime() == 0);
-    // CHECK(defaultItem.getIdleTime() == 0);
-
-    // // Parameterized constructor
-    // Item<int> item1(1, 10, 5, 2);
-    // CHECK(item1.getId() == 1);
-    // CHECK(item1.getOccurTime() == 10);
-    // CHECK(item1.getWorkTime() == 5);
-    // CHECK(item1.getIdleTime() == 2);
-
-    // // Comparison operators
-    // Item<int> item2(2, 15, 3, 7);
-    // CHECK(item1 < item2);
-    // CHECK(item1.compareByOccurTime(item2));
-    // CHECK(item1.compareByIdleTime(item2));
-}
-
-#endif
--- include/problem.h
+++ include/problem.h
@@ -26,8 +26,8 @@ class Problem
 {
 private:
     size_t list_size;               /**< The size of the list. */
-    int machine_amount;
-    std::vector<Item> main_list;
+    int machine_amount;             /**< The number of machines. */
+    std::vector<Item> main_list;    /**< The main list of items. */
 
 public:
     /**
@@ -76,32 +76,85 @@ public:
      * @brief Increments the size of the main list.
      */
     void listSizeIncrement() { this->list_size++; }
+
     /**
      * @brief Decrements the size of the main list.
      */
     void listSizeDecrement() { this->list_size--; }
 
+    /**
+     * @brief Display all items in the list.
+     */
     void displayAll() const;
 
+    /**
+     * @brief Perform permutation sort on the list of items.
+     */
     void permutationSort();
 
+    /**
+     * @brief Apply Johnson's algorithm to the list of items.
+     */
     void johnsonAlgorithm();
 
-    void NEHAlorithm();
+    /**
+     * @brief Apply NEH algorithm to the list of items.
+     */
+    void NEHAlgorithm();
 
-    void FNEHAlorithm();
+    /**
+     * @brief Apply FNEH algorithm to the list of items.
+     */
+    void FNEHAlgorithm();
 
-    double randomDouble(double, double);
+    /**
+     * @brief Generate a random double within a specified range.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return A random double within the specified range.
+     */
+    double randomDouble(double min, double max);
 
-    int objectiveFunction(const std::vector<Item>&);
+    /**
+     * @brief Calculate the objective function for a given order of items.
+     * @param order The order of items.
+     * @return The value of the objective function.
+     */
+    int objectiveFunction(const std::vector<Item>& order);
 
-    std::vector<Item> generateNeighbor(const std::vector<Item>&);
+    /**
+     * @brief Generate a neighbor of the given order of items.
+     * @param order The current order of items.
+     * @return A new order of items representing a neighbor.
+     */
+    std::vector<Item> generateNeighbor(const std::vector<Item>& order);
 
+    /**
+     * @brief Apply simulated annealing algorithm to the list of items.
+     */
     void simulatedAnnealing();
 
-    bool isTabu(const std::vector<Item>&, const std::list<std::vector<Item>>&);
+    /**
+     * @brief Check if a given order of items is in the tabu list.
+     * @param order The order of items.
+     * @param tabu_list The tabu list.
+     * @return true If the order is in the tabu list.
+     * @return false Otherwise.
+     */
+    bool isTabu(const std::vector<Item>& order, const std::list<std::vector<Item>>& tabu_list);
 
+    /**
+     * @brief Apply tabu search algorithm to the list of items.
+     */
     void tabuSearch();
 };
 
-bool operator==(const std::vector<Item<int>>&, const std::vector<Item<int>>&);
+/**
+ * @brief Overloaded equality operator for comparing vectors of items.
+ *
+ * @param lhs The left-hand side vector.
+ * @param rhs The right-hand side vector.
+ * @return true If the vectors are equal.
+ * @return false Otherwise.
+ */
+bool operator==(const std::vector<Item<int>>& lhs, const std::vector<Item<int>>& rhs);
